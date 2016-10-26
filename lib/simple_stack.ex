9028142404b467248d233ec99b981791aa22a6b7
Erik Mejer Hansen <emh@opentelehealth.com>
2016-10-26 13:59:54 +0200

    Can now handle push and pop operations

--- lib/simple_stack.ex
+++ lib/simple_stack.ex
@@ -1,12 +1,47 @@
 defmodule SimpleStack do
   require Logger
 
+
   def start_link() do
-    pid = spawn_link(fn () -> Logger.debug "Started" end)
+    pid = spawn_link(__MODULE__, :receiver, [[]])
+    Process.register(pid, :stack)
     {:ok, pid}
   end
 
-  def push(item) do end
+  def push(element) do
+    send(:stack, {:push, element})
 
-  def pop() do end
+    :ok
+  end
+
+  def pop() do
+    send(:stack, {:pop, self()})
+
+    receive do
+      {:ok, element} ->
+        element
+      _ ->
+        Logger.warn("Unexpected messaged recieved")
+        :error
+    end
+  end
+
+
+  def receiver(state) do
+    state = receive do
+      {:push, element} ->
+        [element | state]
+      {:pop, caller} ->
+
+        [element | state] = state
+        send(caller, {:ok, element})
+
+        state
+      _ ->
+        Logger.warn("Unexpected messaged recieved")
+        state
+    end
+    receiver(state)
+  end
+
 end
